a0c38362788d8d1943b7bbae5b86f5c1890b03ee
pragya <pragyatiwari831@gmail.com>
2022-05-25 14:53:06 +0530

    qwer

--- rough.js
+++ rough.js
@@ -1,2 +1,8 @@
-let a1="cocodede"
-let a2="code"
+let arr = [12, 3, 6, 7, 8]
+let min = arr[0]
+for (let i=0; i<arr.length; i++){
+    if(arr[i] < min){
+        min=arr[i]
+    }
+}
+console.log(min)
